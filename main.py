44e8cbcc084ea97a05058ba8d76ccfbb5df48db6
Jan Leskovec <janleskovec1@gmail.com>
2022-12-29 22:56:26 +0100

    fix log config

--- main.py
+++ main.py
@@ -14,4 +14,4 @@ if __name__ == "__main__":
     # update uvicorn access logger format
     log_config = uvicorn.config.LOGGING_CONFIG
     log_config["formatters"]["access"]["fmt"] = "%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] [trace_id=%(otelTraceID)s span_id=%(otelSpanID)s resource.service.name=%(otelServiceName)s] - %(message)s"
-    uvicorn.run("app.app:app", host="0.0.0.0", port=settings.api_http_port, reload=True, log_level="info")
+    uvicorn.run("app.app:app", host="0.0.0.0", port=settings.api_http_port, reload=True, log_config=log_config)
